ac1e0a26292bf6fd4a06bae555cf936b8e6b09b0
ProphetLamb <prophet.lamb@gmail.com>
2023-05-12 10:16:31 +0200

    Implement removing of func body after match

--- sync.ts
+++ sync.ts
@@ -1,7 +1,6 @@
 // Pull remote files over local files
 // Apply patches to remote files
 // Attempt to compile the patched files
-// required progress and @types/progress
 
 import * as fs from 'fs';
 import * as path from 'path';
@@ -9,13 +8,30 @@ import * as https from 'https';
 import ProgressBar from 'progress';
 
 class Patch {
-	constructor(public readonly remotePath: string, public readonly patches?: { regex: RegExp, replace: string }[] | undefined) { }
+	constructor(public readonly remotePath: string, public readonly patches?: { regex: RegExp, replace: string, removeBodyAfterMatch?: boolean }[] | undefined) { }
 }
 
 const baseUrl = "https://raw.githubusercontent.com/microsoft/vscode/main/"
 const fileMappings: { [key: string]: Patch } = {
 	"tsconfig.base.json" : new Patch("extensions/tsconfig.base.json"),
-	"src/launcher.ts" : new Patch("extensions/markdown-language-features/src/extension.ts"),
+	"src/launcher.ts" : new Patch("extensions/markdown-language-features/src/extension.ts", [
+		// replace: import { IMdParser, MarkdownItEngine } from './markdownEngine';
+		// with: 		import { IMdParser } from 'vscode-markdown-languageservice';
+		{ regex: /import { IMdParser, MarkdownItEngine } from '\.\/markdownEngine';/g, replace: "import { IMdParser } from 'vscode-markdown-languageservice';" },
+		// remove line: import { activateShared } from './extension.shared';
+		{ regex: /import { activateShared } from '\.\/extension\.shared';\n/g, replace: "" },
+		// remove line: import { VsCodeOutputLogger } from './logging';
+		{ regex: /import { VsCodeOutputLogger } from '\.\/logging';\n/g, replace: "" },
+		// remove line: import { getMarkdownExtensionContributions } from './markdownExtensions';
+		{ regex: /import { getMarkdownExtensionContributions } from '\.\/markdownExtensions';\n/g, replace: "" },
+		// remove line: import { githubSlugifier } from './slugify';
+		{ regex: /import { githubSlugifier } from '\.\/slugify';\n/g, replace: "" },
+		// replace: function startServer(context: vscode.ExtensionContext, parser: IMdParser): Promise<MdLanguageClient> {
+		// with:		export function startServer(context: vscode.ExtensionContext, parser: IMdParser): Promise<MdLanguageClient> {
+		{ regex: /function startServer\(context: vscode\.ExtensionContext, parser: IMdParser\): Promise<MdLanguageClient> {/g, replace: "export function startServer(context: vscode.ExtensionContext, parser: IMdParser): Promise<MdLanguageClient> {" },
+		// remove function: export async function activate(context: vscode.ExtensionContext) { ... }
+		{ regex: /export async function activate\(context: vscode\.ExtensionContext\)\s*/gm, replace: "", removeBodyAfterMatch: true },
+	]),
 	"src/client/client.ts" : new Patch("extensions/markdown-language-features/src/client/client.ts"),
 	"src/client/fileWatchingManager.ts": new Patch("extensions/markdown-language-features/src/client/fileWatchingManager.ts"),
 	"src/client//inMemoryDocument.ts" : new Patch("extensions/markdown-language-features/src/client/inMemoryDocument.ts"),
@@ -45,10 +61,42 @@ function downloadFile(url: string): Promise<Buffer> {
 	});
 }
 
-function applyPatches(file: Buffer, patches: { regex: RegExp, replace: string }[]): string {
+function applyPatches(file: Buffer, patches: { regex: RegExp, replace: string, removeBodyAfterMatch?: boolean }[]): string {
+	function matchNestedBrackets(str: string, start: number): number {
+		let depth = 0;
+		for (let i = start; i < str.length; i++) {
+			if (str[i] === '{') {
+				depth++;
+			} else if (str[i] === '}') {
+				depth--;
+				if (depth === 0) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+	function removeMatchAndNestedBrackets(result: string, match: RegExpExecArray): string {
+		const start = match.index + match.length;
+		const end = matchNestedBrackets(result, start);
+		if (end === -1) {
+			throw new Error("Could not find matching bracket");
+		}
+		return result.substring(0, match.index) + result.substring(end + 1);
+	}
+
 	let result = file.toString();
 	for (const patch of patches) {
-		result = result.replace(patch.regex, patch.replace);
+		if (!patch.removeBodyAfterMatch) {
+			result = result.replace(patch.regex, patch.replace);
+		} else {
+			// after each match, find bracket pairs and remove them
+			// also remove the match itself
+			let match: RegExpExecArray | null;
+			while (match = patch.regex.exec(result)) {
+				result = removeMatchAndNestedBrackets(result, match);
+			}
+		}
 	}
 	return result;
 }
